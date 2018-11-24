87e31722886d356295f4387383d528020bc7cd88
Theo Sun <theo.sun@outlook.com>
2018-11-24 21:01:50 +0800

    chore(release): new version

--- release.sh
+++ release.sh
@@ -4,12 +4,13 @@ if [ -z "$1" ]
 then
   echo "must give out a new version"
 else
-  git-chglog -o CHANGELOG.md
   git add -A
   git commit -m "chore(release): new version"
   git tag $1
+  git-chglog -o CHANGELOG.md
+  git add -A
+  git commit -m "chore(release): update CHANGELOG"
   git push --tags
+  echo "new version $1 released"
 fi
-
-
 
