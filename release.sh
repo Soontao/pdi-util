8b094cbdb4258e607f0e02f5cee5f5af5b82b71d
Theo Sun <theo.sun@outlook.com>
2019-04-19 09:22:41 +0800

    chore(script): update

--- release.sh
+++ release.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# check golang installed
+if ! [ -x "$(command -v go)" ]; then
+  echo 'Error: golang is not installed.' >&2
+  exit 1
+fi
+
+# install git-chglog if not exist
+if ! [ -x "$(command -v git-chglog)" ]; then
+  echo "install git chglog..."
+  go get -u github.com/git-chglog/git-chglog/cmd/git-chglog
+fi
+
+# ensure a new tag assigned
 if [ -z "$1" ]
 then
   echo "must give out a new version"
